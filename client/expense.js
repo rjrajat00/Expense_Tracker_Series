3997a0295c495f636975a2866ea56a90390cc2ed
Rajat SIngh <rjrajat00@gmail.com>
2024-01-11 09:34:34 +0530

    Report Generation Frontend (Premium Feature)

--- client/expense.js
+++ client/expense.js
@@ -308,4 +308,24 @@ document.addEventListener("DOMContentLoaded", async () => {
       // Handle the error (display a message, redirect, etc.)
     }
   });
+
+  function download() {
+    axios
+      .get("/user/download", { headers: { Authorization: token } })
+      .then((response) => {
+        if (response.status === 201) {
+          //the bcakend is essentially sending a download link
+          //  which if we open in browser, the file would download
+          var a = document.createElement("a");
+          a.href = response.data.fileUrl;
+          a.download = "myexpense.csv";
+          a.click();
+        } else {
+          throw new Error(response.data.message);
+        }
+      })
+      .catch((err) => {
+        showError(err);
+      });
+  }
 });
